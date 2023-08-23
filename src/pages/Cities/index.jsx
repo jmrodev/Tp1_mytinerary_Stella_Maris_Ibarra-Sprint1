42b112674446cbddb8dd227b2ea66d51913e3f3a
stellamibarra <stellamaristandil@gmail.com>
2023-08-23 13:22:17 -0300

    backup antes de search

--- src/pages/Cities/index.jsx
+++ src/pages/Cities/index.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useRef } from "react";
 import CardCity from "../../components/CardCity/Index.jsx";
 import { getAllCities } from "../services/cityService.js";
 import CityDetails from "../CitiesDetail/Index.jsx";
 
 const Cities = () => {
   const [cities, setCities] = useState([]);
+
+  const select = useRef(null);
+  const inputBusqueda = useRef(null);
 
   useEffect(() => {
     // fetch("http://localhost:3000/api/cities")
@@ -16,6 +19,19 @@ const Cities = () => {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleInput = () => {
+    const city = select.current.value;
+    const search = inputBusqueda.current.value;
+    let query = `?`;
+    if (city !== "All") {
+      query += "city=" + city + "&";
+    }
+    if (search) {
+      query += "name=" + search;
+    }
+    getAllCities(query).then(setCities);
+  };
+
   return (
     
     <section className="flex gap-5 flex-wrap p-10 mt-6 justify-content-center 	">
@@ -25,5 +41,8 @@ const Cities = () => {
     </section>
   );
 };
+
+
+
 
 export default Cities;
