95ea35a789f51ebc8f986c0bc45989f9693021d5
stellamibarra <stellamaristandil@gmail.com>
2023-08-30 14:45:49 -0300

    redux

--- src/pages/Cities/index.jsx
+++ src/pages/Cities/index.jsx
@@ -2,16 +2,27 @@ import React, { useEffect, useState } from "react";
 import CardCity from "../../components/CardCity/Index.jsx";
 import { getAllCities } from "../services/cityService.js";
 import CityDetails from "../../components/CitiesDetail/Index.jsx";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { citiesActions } from "../../redux/actions/citiesActions.js";
 
 const Cities = () => {
   const [cities, setCities] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [quantity, setQuantity] = useState(15); // Cantidad de ciudades a mostrar
 
+  const citiesInstore = useSelector(store => store.citiesReducer.cities);
+  console.log(citiesInstore);
+
+  const dispatch = useDispatch ()
+
   useEffect(() => {
     getAllCities().then((response) => {
+
       setCities(response);
+      dispatch (citiesActions.add_cities(response.data))
     });
+
   }, []);
   
   const handleSearchChange = (event) => {
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import citiesReducer from "./reducers/cities";
 
  export const store = configureStore({
+  //dentro del store agrego los reducers de cities y users
+
    reducer: {
-     // counter: counterReducer,
-     // user: userReducer,
+      citiesReducer: citiesReducer,
+      
 
 
    }, 
